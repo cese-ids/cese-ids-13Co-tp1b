13dbea6d74bdca9e840d07669eb90c37f57d7723
Santiago Almandos <santiagoalman@gmail.com>
2020-12-01 22:00:20 -0300

    Se agregan los datos del alumno ALMANDOS MARINZULICH, Santiago Nicolas

--- src/alumnos.c
+++ src/alumnos.c
@@ -31,8 +31,15 @@ static const struct alumno_s ESTEBAN_VOLENTINI = {
     .documento = "23.517.968",
 };
 
+static const struct alumno_s SANTIAGO_ALMANDOS = {
+    .apellidos = "ALMANDOS MARINZULICH",
+    .nombres = "Santiago Nicolas",
+    .documento = "93.748.537",
+};
+
 const alumno_t ALUMNOS[] = {
     &ESTEBAN_VOLENTINI,
+    &SANTIAGO_ALMANDOS,
 };
 
 const int CANTIDAD_ALUMNOS = (sizeof(ALUMNOS) / sizeof(alumno_t));

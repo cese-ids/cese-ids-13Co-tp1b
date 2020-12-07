c51b6afee04e177d2d5ad6084ea1c50805eb819e
Ariel Cerfoglia <ariel.cerfoglia@gmail.com>
2020-12-07 18:06:41 -0300

    Se agregan los datos de CERFOGLIA, Ariel.

--- src/alumnos.c
+++ src/alumnos.c
@@ -25,14 +25,14 @@
 
 /*=====[Definitions of public global variables]==============================*/
 
-static const struct alumno_s ESTEBAN_VOLENTINI = {
-    .apellidos = "VOLENTINI",
-    .nombres = "Esteban Daniel",
-    .documento = "23.517.968",
+static const struct alumno_s ARIEL_CERFOGLIA = {
+    .apellidos = "CERFOGLIA",
+    .nombres = "Ariel Alejandro",
+    .documento = "36.273.467",
 };
 
 const alumno_t ALUMNOS[] = {
-    &ESTEBAN_VOLENTINI,
+    &ARIEL_CERFOGLIA,
 };
 
 const int CANTIDAD_ALUMNOS = (sizeof(ALUMNOS) / sizeof(alumno_t));
